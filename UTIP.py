2bb5f5ab304a03fcb722572b86bdfab711a6487e
Hannanah Mobashir <hanana.m@gmail.com>
2017-09-20 13:54:07 -0400

    feat: question4
    Added question, explanation, test cases and solution for question4.

--- UTIP.py
+++ UTIP.py
@@ -79,3 +79,55 @@ def question2(a):
 # entire string is palindrome i.e. 'alevela'
 # string has only one palindrome i.e. 'banana'
 # string has multipl palindromes i.e. 'annalevels'
+
+##############
+# QUESTION 4 #
+##############
+
+# My understanding of the question:
+# Find Least Common Ancestor of two nodes in a BST
+# BST is represented by a 2D matrix
+# Each node is an integer
+# Both of the given nodes are always in BST
+# T = tree, r = int value of root, n1 and n2 are non-negative integers
+
+# Why I approached the question this way:
+# I wanted to avoid nested loops leading to greater time
+# Simple solution run recursively
+# Build upon the fact that BST is sorted
+
+def question4(T, r, n1, n2):
+    print ' '
+    # check if n1 and n2 are on different sides of the root
+    if (n1 > r and n2 < r) or (n1 < r and n2 > r):
+        # return root if n1 and n2 are on different sides
+        # because in this case the root is the only common ancestor
+        return r
+    # check if both n1 and n2 are smaller than root
+    elif (n1 < r and n2 < r):
+        left = T[r].index(1)
+        # check if n1 or n2 equals left child of root
+        if n1 == left or n2 == left:
+            return r
+        # change root to left child
+        else:
+            r = left
+    # check if both n1 and n2 are greater than root
+    elif (n1 > r and n2 > r):
+        right = len(T[r]) - T[r][::-1].index(1) - 1
+        # check if n1 or n2 equals right child of root
+        if n1 == right or n2 == right:
+            return r
+        else:
+            # change root to right child
+            r = right
+            
+    # run recursively with updated root
+    return question4(T, r, n1, n2)
+
+# test cases
+# Given nodes are children of same parent
+# Given nodes are children of different parents
+# Given nodes have a parent child relationship
+# Given nodes are on same side of BST
+# Given nodes are on different sides of BST
